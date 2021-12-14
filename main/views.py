1119f8d685bbaf862e170c478398df8d447824a1
jose-izam99 <57118197+jose-izam99@users.noreply.github.com>
2021-12-14 16:53:29 -0300

    juego

--- main/views.py
+++ main/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def games(request):
+    return render(request, "games.html")
