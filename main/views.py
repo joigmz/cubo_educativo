96235db3a2b38bdb7a22dd625e8b4b3a2f45876d
jose-izam99 <57118197+jose-izam99@users.noreply.github.com>
2021-12-14 18:25:50 -0300

    juego

--- main/views.py
+++ main/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
+from pathlib import Path
+import os
+import numpy as np
 
 # Create your views here.
 
-# Create your views here.
 def index(request):
     return render(request, "index.html")
 
 def games(request):
-    return render(request, "games.html")
+
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    games_paths = os.path.join(BASE_DIR,'static/Proyectos_yo_programo.txt')
+
+    with open(games_paths, 'r') as file:
+        data = file.read().replace('\n\n', ': ')
+
+    data = data.split(": ")
+    links, nombres = [], []
+
+    for i in range(len(data)):
+        if i%2 != 0:
+            links.append(data[i])
+        else:
+            nombres.append(data[i])
+
+    links, nombres = np.array(links), np.array(nombres)
+
+    context = {
+       'links' : links[:5],
+       'nombres': nombres[:5]
+    }
+
+    return render(request, "games.html", context=context)
